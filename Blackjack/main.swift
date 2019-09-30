b9e5b9b2011e2ad1219da5c986fc31d3dc876444
Will Murat Said <will.said7@gmail.com>
2019-09-29 23:03:13 -0700

    first commit

---
+++ Blackjack/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Blackjack
+//
+//  Created by Will Said on 9/29/19.
+//  Copyright © 2019 Will Said. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
